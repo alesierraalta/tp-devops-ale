f9b22a50a901390b72bb8aa5ce32e7701697ad2d
alesierra <alesierraalta@gmail.com>
2023-11-27 15:43:15 -0400

    github actions correct v3

--- tests/test_app.py
+++ tests/test_app.py
@@ -4,21 +4,23 @@ import os
 # from flask_sqlalchemy import SQLAlchemy
 from app import app, db, Usuario, Nota
 
-# Add the application directory to sys.path
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), os.path.pardir)))
+# Add the app directory to sys.path
+sys.path.append(os.path.abspath(os.path.join(
+    os.path.dirname(__file__), os.path.pardir)))
 
 
 class FlaskBlogTestCase(unittest.TestCase):
 
     def setUp(self):
         app.config['TESTING'] = True
-        app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///:memory:'
+        app.config['SQLALCHEMY_DATABASE_URI'] = \
+            'sqlite:///:memory:'
         app.config['WTF_CSRF_ENABLED'] = False
         self.app = app.test_client()
 
         with app.app_context():
             db.create_all()
-            # Create and add a test user to the database
+            # Create/test user in database
             usuario_prueba = Usuario(username='testuser')
             usuario_prueba.set_password('testpassword')
             db.session.add(usuario_prueba)
@@ -43,33 +45,44 @@ class FlaskBlogTestCase(unittest.TestCase):
     def test_login_logout(self):
         rv = self.login()
         response_text = rv.data.decode('utf-8')
-        print("Response after logging in:", response_text)  # Add print for debugging
-        assert 'Welcome, testuser' in response_text or 'Logout' in response_text
+        # Add print for debugging
+        print("Response after logging in:", response_text)
+        assert 'Welcome, testuser' in response_text \
+            or 'Logout' in response_text
         rv = self.logout()
         response_text = rv.data.decode('utf-8')
-        print("Response after logging out:", response_text)  # Add print for debugging
-        assert 'Welcome, testuser' not in response_text and 'Logout' not in response_text
+        # Add print for debugging
+        print("Response after logging out:", response_text)
+        assert 'Welcome, testuser' not in response_text \
+            and 'Logout' not in response_text
 
     def test_edit_note(self):
         # Test to verify note editing
         with app.app_context():
             self.login()
-            self.app.post('/add', data={'note': 'Note to edit'}, follow_redirects=True)
+            self.app.post('/add', data={'note': 'Note to edit'},
+                          follow_redirects=True)
             note = Nota.query.filter_by(content='Note to edit').first()
-            rv = self.app.post(f'/edit/{note.id}', data={'note': 'Edited note'}, follow_redirects=True)
+            rv = self.app.post(f'/edit/{note.id}',
+                               data={'note': 'Edited note'},
+                               follow_redirects=True)
             response_text = rv.data.decode('utf-8')
-            print("Response after editing note:", response_text)  # Add print for debugging
+            # Add print for debugging
+            print("Response after editing note:", response_text)
             assert b'Edited note' in rv.data
 
     def test_delete_note(self):
         # Test to verify note deletion
         with app.app_context():
             self.login()
-            self.app.post('/add', data={'note': 'Note to delete'}, follow_redirects=True)
+            self.app.post('/add', data={'note': 'Note to delete'},
+                          follow_redirects=True)
             note = Nota.query.filter_by(content='Note to delete').first()
-            rv = self.app.get(f'/delete/{note.id}', follow_redirects=True)
+            rv = self.app.get(f'/delete/{note.id}',
+                              follow_redirects=True)
             response_text = rv.data.decode('utf-8')
-            print("Response after deleting note:", response_text)  # Add print for debugging
+            # Add print for debugging
+            print("Response after deleting note:", response_text)
             assert b'Note to delete' not in rv.data
 
 if __name__ == '__main__':
