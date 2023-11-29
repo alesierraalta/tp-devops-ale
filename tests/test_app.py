6ed10bc5a0cb5321e90af0b773213410386f016b
alesierra <alesierraalta@gmail.com>
2023-11-29 14:43:17 -0400

    estable metricas 1.5

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,11 +1,11 @@
 import unittest
 import sys
 import os
-
+from app import app, db, Usuario, Nota
 # Ajustar las importaciones al inicio del archivo
 sys.path.append(os.path.abspath(os.path.join(
     os.path.dirname(__file__), os.path.pardir)))
-from app import app, db, Usuario, Nota
+
 
 
 class FlaskBlogTestCase(unittest.TestCase):
