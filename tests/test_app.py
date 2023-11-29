6b67045ac2c4b114a9a5e25fd6636208ec348b36
alesierra <alesierraalta@gmail.com>
2023-11-29 19:24:57 -0400

    prueba presentacion

--- tests/test_app.py
+++ tests/test_app.py
@@ -2,7 +2,7 @@ import unittest
 import sys
 import os
 from app import app, db, Usuario, Nota
-# Ajustar las importaciones al inicio del archivo
+# Ajustar las importaciones al inicio del archivooooo
 sys.path.append(os.path.abspath(os.path.join(
     os.path.dirname(__file__), os.path.pardir)))
 
